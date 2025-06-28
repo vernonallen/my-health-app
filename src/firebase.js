e10a2ee5f14737777d949e85a40548883ca01b0c
anthonyallen-sands <90800864+anthonyallen-sands@users.noreply.github.com>
2025-06-28 21:06:03 +0800

    Fix errors in firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyB3veznhHhELkYqQWqqnCWWdntdKjugoUA",
+  authDomain: "my-health-agent.firebaseapp.com",
+  projectId: "my-health-agent",
+  storageBucket: "my-health-agent.firebasestorage.app,
+  messagingSenderId: "575524026210",
+  appId: "1:575524026210:web:0853b9a084a8cb086c9e3b",
+  measurementId: "G-KYBWHRF2E9"
 };
 
 const app = initializeApp(firebaseConfig);
