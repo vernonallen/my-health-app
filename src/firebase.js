b0190b307d9b27a1d588792a41d1473c141c4da2
anthonyallen-sands <90800864+anthonyallen-sands@users.noreply.github.com>
2025-06-29 08:57:44 +0800

    Fix typo in firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyB3veznhHhELkYqQWqqnCWWdntdKjugoUA",
   authDomain: "my-health-agent.firebaseapp.com",
   projectId: "my-health-agent",
-  storageBucket: "my-health-agent.firebasestorage.app,
+  storageBucket: "my-health-agent.firebasestorage.app",
   messagingSenderId: "575524026210",
   appId: "1:575524026210:web:0853b9a084a8cb086c9e3b",
   measurementId: "G-KYBWHRF2E9"
